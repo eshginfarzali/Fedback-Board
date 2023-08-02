785c33d4d675f19476fe07ccc1bab58f63e3623d
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-08-02 23:49:30 +0400

    feat: nav menu and responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,16 @@ const allBox = document.querySelector('.all-box');
 menuToggle.addEventListener('change', function() {
     if (this.checked) {
         body.style.overflow = 'hidden';
+        if (window.matchMedia("(max-width: 768px)").matches) {
         roadmapBox.style.display = 'inline-block';
         allBox.style.display = 'inline-block';
+        }
     } else {
         body.style.overflow = 'auto';
+        if (window.matchMedia("(max-width: 768px)").matches) {
         roadmapBox.style.display = 'none';
         allBox.style.display = 'none';
+        location.reload();
+        }
     }
 });
