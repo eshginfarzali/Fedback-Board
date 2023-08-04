a2e421bc8972a8fb70d0bd056a2bd90ba84c3c78
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-08-05 01:25:08 +0400

    fix main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ const enhancement = document.querySelector(".enhancement");
 const bug = document.querySelector(".bug");
 const feature = document.querySelector(".feature");
 
+
 async function fetchAndRenderData() {
   try {
     const response = await fetch("../../Json/feedback.json");
@@ -19,38 +20,85 @@ async function fetchAndRenderData() {
     }
 
     const data = await response.json();
-    feedBackBox.innerHTML = ""; 
+    feedBackBox.innerHTML = "";
 
     data.forEach((item) => {
-      feedBackBox.innerHTML += `
-        <section class="add-feedback-box">
-          <div class="count-feed">
-            <button>
-              <img src="./assets/icons/chevron-up.svg" alt="up" /> <br />
-              ${item.reiting}
-            </button>
-          </div>
-    
-          <div class="text-feed-box">
-            <div class="text-feed">
-              <h2>${item.title}</h2>
-              <p>
-                ${item.text}
-              </p>
-              <button>${item.category}</button>
-            </div>
-            <div class="feed-com">
-              <img src="./assets/icons/comment.svg" alt="comment" />
-              <span>${item.comment}</span>
-            </div>
-          </div>
-        </section>
-      `;
+      const section = document.createElement("section");
+      section.className = "add-feedback-box";
+
+      const countFeedDiv = document.createElement("div");
+      countFeedDiv.className = "count-feed";
+
+      const countButton = document.createElement("button");
+      const countImg = document.createElement("img");
+      countImg.src = "./assets/icons/chevron-up.svg";
+      countImg.alt = "up";
+
+      const countReitin = document.createElement("span");
+      countReitin.className = "countReitin";
+      countReitin.textContent = item.reiting;
+
+      countButton.appendChild(countImg);
+      countButton.appendChild(document.createElement("br"));
+      countButton.appendChild(countReitin);
+      countFeedDiv.appendChild(countButton);
+
+      const textFeedBoxDiv = document.createElement("div");
+      textFeedBoxDiv.className = "text-feed-box";
+
+      const textFeedDiv = document.createElement("div");
+      textFeedDiv.className = "text-feed";
+
+      const titleHeading = document.createElement("h2");
+      titleHeading.textContent = item.title;
+
+      const textParagraph = document.createElement("p");
+      textParagraph.textContent = item.text;
+
+      const categoryButton = document.createElement("button");
+      categoryButton.textContent = item.category;
+
+      textFeedDiv.appendChild(titleHeading);
+      textFeedDiv.appendChild(textParagraph);
+      textFeedDiv.appendChild(categoryButton);
+
+      const feedComDiv = document.createElement("div");
+      feedComDiv.className = "feed-com";
+
+      const commentImg = document.createElement("img");
+      commentImg.src = "./assets/icons/comment.svg";
+      commentImg.alt = "comment";
+
+      const commentSpan = document.createElement("span");
+      commentSpan.textContent = item.comment;
+
+      feedComDiv.appendChild(commentImg);
+      feedComDiv.appendChild(commentSpan);
+
+      textFeedBoxDiv.appendChild(textFeedDiv);
+      textFeedBoxDiv.appendChild(feedComDiv);
+
+      section.appendChild(countFeedDiv);
+      section.appendChild(textFeedBoxDiv);
+
+      countButton.addEventListener("click", () => {
+        item.reiting++;
+        countReitin.textContent = item.reiting.toString();
+      });
+
+      feedBackBox.appendChild(section);
     });
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
+
+fetchAndRenderData();
+
+
+
+
+
 
 function filterAndDisplay(category) {
   const feedbackSections = feedBackBox.querySelectorAll(".add-feedback-box");
