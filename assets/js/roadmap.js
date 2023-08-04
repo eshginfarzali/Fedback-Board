28c0431e8a89349f95d58413630b6f22b9353577
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-08-04 19:23:43 +0400

    fix drag drop

--- assets/js/roadmap.js
+++ assets/js/roadmap.js
@@ -1,50 +1,86 @@
-// Custom Dragula JS
-var drake = dragula([
+const drake = dragula([
     document.getElementById("planned"),
     document.getElementById("progress"),
     document.getElementById("live"),
+    document.getElementById("empty-planned"), // New drop target
+    document.getElementById("empty-progress"), // New drop target
+    document.getElementById("empty-live") // New drop target
   ], {
     removeOnSpill: false
   });
   
   drake
-    .on("drag", function(el) {
-      el.classList.remove("ex-moved");
-    })
-    .on("drop", function(el) {
-      el.classList.add("ex-moved");
-    })
-    .on("over", function(el, container) {
-      container.classList.add("ex-over");
-    })
-    .on("out", function(el, container) {
-      container.classList.remove("ex-over");
-    });
-  
-  // Vanilla JS to add a new task
-  function addTask() {
-    var inputTask = document.getElementById("taskText").value;
-    var newTask = document.createElement("li");
-    newTask.className = "task";
-    newTask.innerHTML = "<p>" + inputTask + "</p>";
+  .on("drag", function(el) {
+    el.classList.remove("ex-moved");
+  })
+  .on("drop", function(el, target, source) {
+    el.classList.add("ex-moved");
+    if (target.classList.contains("empty-container")) {
+      const status = target.getAttribute("data-status");
+      el.setAttribute("data-status", status);
+      target.appendChild(el);
+    }
+  })
+  .on("over", function(_, container) {
+    container.classList.add("ex-over");
+  })
+  .on("out", function(_, container) {
+    container.classList.remove("ex-over");
+  });
+
     
-    document.getElementById("planned").appendChild(newTask);
-    document.getElementById("taskText").value = "";
-  }
+      async function fetchAndRenderData() {
+        try {
+          const response = await fetch("../../Json/feedback.json");
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+      
+          const data = await response.json();
+          data.forEach((item) => {
+            const listItem = document.createElement("li");
+            listItem.classList.add("task");
+            listItem.setAttribute("data-status", item.status);
+      
+            listItem.innerHTML = `
+              <div class="status">
+                <div class="status-color" style="background-color: ${item.statusColor}"></div>
+                &ThinSpace;
+                <p class="status-name">${item.status}</p>
+              </div>
+              <div class="title">${item.title}</div>
+              <p class="text">
+                ${item.text}
+              </p>
+              <div class="category"><span>${item.category}</span></div>
+              <div class="reyting-comment">
+                <button class="reyting">
+                  <img src="../icons/chevron-up.svg" alt="" />
+                  <span>123</span>
+                </button>
+                <div class="comment">
+                  <img src="../icons/comment.svg" alt="" /> <span>2</span>
+                </div>
+              </div>
+            `;
+      
+            if (item.status === "Planned") {
+              planned.appendChild(listItem);
+            } else if (item.status === "In-Progress") {
+              progress.appendChild(listItem);
+            } else if (item.status === "Live") {
+              live.appendChild(listItem);
+            }
+          });
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+      
+    fetchAndRenderData();
+    
   
   
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
