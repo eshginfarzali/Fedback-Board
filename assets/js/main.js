f3cc0c9aef61848b4a770fa5e7d17d2530896ce3
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-08-05 00:48:21 +0400

    feat: write feedback data index.html write

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,89 @@ const body = document.body;
 const menuToggle = document.getElementById('menu__toggle');
 const roadmapBox = document.querySelector('.roadmap-box');
 const allBox = document.querySelector('.all-box');
-const addFeedbackBox =document.querySelector('.add-feedback-box')
+const feedBackBox = document.getElementById("feedBackBox"); 
+
+const all = document.querySelector(".all");
+const ui = document.querySelector(".ui");
+const ux = document.querySelector(".ux");
+const enhancement = document.querySelector(".enhancement"); 
+const bug = document.querySelector(".bug");
+const feature = document.querySelector(".feature");
+
+async function fetchAndRenderData() {
+  try {
+    const response = await fetch("../../Json/feedback.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    feedBackBox.innerHTML = ""; 
+
+    data.forEach((item) => {
+      feedBackBox.innerHTML += `
+        <section class="add-feedback-box">
+          <div class="count-feed">
+            <button>
+              <img src="./assets/icons/chevron-up.svg" alt="up" /> <br />
+              ${item.reiting}
+            </button>
+          </div>
+    
+          <div class="text-feed-box">
+            <div class="text-feed">
+              <h2>${item.title}</h2>
+              <p>
+                ${item.text}
+              </p>
+              <button>${item.category}</button>
+            </div>
+            <div class="feed-com">
+              <img src="./assets/icons/comment.svg" alt="comment" />
+              <span>${item.comment}</span>
+            </div>
+          </div>
+        </section>
+      `;
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+function filterAndDisplay(category) {
+  const feedbackSections = feedBackBox.querySelectorAll(".add-feedback-box");
+  feedbackSections.forEach((section) => {
+    const categoryButton = section.querySelector(".text-feed button");
+    if (categoryButton.textContent === category || category === "all") {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  });
+}
+
+all.addEventListener("click", () => filterAndDisplay("all"));
+ui.addEventListener("click", () => filterAndDisplay("UI"));
+ux.addEventListener("click", () => filterAndDisplay("UX"));
+enhancement.addEventListener("click", () => filterAndDisplay("Enhancement"));
+bug.addEventListener("click", () => filterAndDisplay("Bug"));
+feature.addEventListener("click", () => filterAndDisplay("Feature"));
+
+fetchAndRenderData();
+
+
+ 
+
+
+
+
+
+
+
+
+
+
 
 function updateLayout() {
   if (window.matchMedia("(max-width: 768px)").matches) {
@@ -29,6 +111,7 @@ window.addEventListener('resize', updateLayout);
 updateLayout();
 
 
-addFeedbackBox.addEventListener("click", ()=>{
-    location.href = "./assets/page/feedback-detial.html"
-})
+// addFeedbackBox.addEventListener("click", ()=>{
+//     location.href = "./assets/page/feedback-detial.html"
+// })
+
--- assets/js/roadmap.js
+++ assets/js/roadmap.js
@@ -1,128 +1,142 @@
 const drake = dragula([
-    document.getElementById("planned"),
-    document.getElementById("progress"),
-    document.getElementById("live"),
-    document.getElementById("empty-planned"), // New drop target
-    document.getElementById("empty-progress"), // New drop target
-    document.getElementById("empty-live") // New drop target
-  ], {
-    removeOnSpill: false
-  });
-  
-  drake
-  .on("drag", function(el) {
-    el.classList.remove("ex-moved");
-  })
-  .on("drop", function(el, target, source) {
-    el.classList.add("ex-moved");
-    if (target.classList.contains("empty-container")) {
-      const status = target.getAttribute("data-status");
-      el.setAttribute("data-status", status);
-      target.appendChild(el);
-    }
-  })
-  .on("over", function(_, container) {
-    container.classList.add("ex-over");
-  })
-  .on("out", function(_, container) {
-    container.classList.remove("ex-over");
-  });
+  document.getElementById("planned"),
+  document.getElementById("progress"),
+  document.getElementById("live"),
+  document.getElementById("empty-planned"), 
+  document.getElementById("empty-progress"), 
+  document.getElementById("empty-live") 
+], {
+  removeOnSpill: false
+});
 
-    
-      async function fetchAndRenderData() {
-        try {
-          const response = await fetch("../../Json/feedback.json");
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-      
-          const data = await response.json();
-          data.forEach((item) => {
-            const listItem = document.createElement("li");
-            listItem.classList.add("task");
-            listItem.setAttribute("data-status", item.status);
-      
-            listItem.innerHTML = `
-              <div class="status">
-                <div class="status-color" style="background-color: ${item.statusColor}"></div>
-                &ThinSpace;
-                <p class="status-name">${item.status}</p>
-              </div>
-              <div class="title">${item.title}</div>
-              <p class="text">
-                ${item.text}
-              </p>
-              <div class="category"><span>${item.category}</span></div>
-              <div class="reyting-comment">
-                <button class="reyting">
-                  <img src="../icons/chevron-up.svg" alt="" />
-                  <span>123</span>
-                </button>
-                <div class="comment">
-                  <img src="../icons/comment.svg" alt="" /> <span>2</span>
-                </div>
-              </div>
-            `;
-      
-            if (item.status === "Planned") {
-              planned.appendChild(listItem);
-            } else if (item.status === "In-Progress") {
-              progress.appendChild(listItem);
-            } else if (item.status === "Live") {
-              live.appendChild(listItem);
-            }
-          });
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
+drake
+.on("drag", function(el) {
+  el.classList.remove("ex-moved");
+})
+.on("drop", function(el, target, source) {
+  el.classList.add("ex-moved");
+
+  if (target.classList.contains("empty-container")) {
+    const oldStatus = el.getAttribute("data-status");
+    const newStatus = target.getAttribute("data-status");
+
+    if (oldStatus !== newStatus) {
+      updateListItem(el, target);
+      source.removeChild(el);
+    }
+  }
+})
+.on("over", function(_, container) {
+  container.classList.add("ex-over");
+})
+.on("out", function(_, container) {
+  container.classList.remove("ex-over");
+});
+
+function updateListItem(item, target) {
+  const status = target.getAttribute("data-status");
+  const statusColor = target.getAttribute("data-status-color");
+  const statusName = target.getAttribute("data-status-name");
+  
+  item.setAttribute("data-status", status);
+  item.querySelector(".status-color").style.backgroundColor = statusColor;
+  item.querySelector(".status-name").textContent = statusName;
+  
+  target.appendChild(item);
+}
+
+
+async function fetchAndRenderData() {
+  try {
+    const response = await fetch("../../Json/feedback.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    data.forEach((item) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("task");
+      listItem.setAttribute("data-status", item.status);
+
+      listItem.innerHTML = `
+        <div class="status">
+          <div class="status-color" style="background-color: ${item.statusColor}" data-status-color="${item.statusColor}" data-status-name="${item.status}">
+          </div>
+          &ThinSpace;
+          <p class="status-name">${item.status}</p>
+        </div>
+        <div class="title">${item.title}</div>
+        <p class="text">
+          ${item.text}
+        </p>
+        <div class="category"><span>${item.category}</span></div>
+        <div class="reyting-comment">
+          <button class="reyting">
+            <img src="../icons/chevron-up.svg" alt="" />
+            <span>123</span>
+          </button>
+          <div class="comment">
+            <img src="../icons/comment.svg" alt="" /> <span>2</span>
+          </div>
+        </div>
+      `;
+
+      if (item.status === "Planned") {
+        planned.appendChild(listItem);
+      } else if (item.status === "In-Progress") {
+        progress.appendChild(listItem);
+      } else if (item.status === "Live") {
+        live.appendChild(listItem);
       }
-      
-    fetchAndRenderData();
-    
-  
-  
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+fetchAndRenderData();
+
+
+
+
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    const plannedHeader = document.querySelector('.planned-header');
-    const inProgressHeader = document.querySelector('.in-progress-header');
-    const liveHeader = document.querySelector('.live-header');
-    const columns = document.querySelector('.columns');
-    columns.classList.remove('show-in-progress', 'show-live');
+  const plannedHeader = document.querySelector('.planned-header');
+  const inProgressHeader = document.querySelector('.in-progress-header');
+  const liveHeader = document.querySelector('.live-header');
+  const columns = document.querySelector('.columns');
 
-      //border add 
-    plannedHeader.classList.add('show-border-planned');
+  // İlk yüklendiğinde yalnızca "Planned" sütunu görünsün
+  columns.classList.add('show-planned');
+  plannedHeader.classList.add('show-border-planned');
 
-    plannedHeader.addEventListener('click', function () {
+  plannedHeader.addEventListener('click', function () {
       columns.classList.remove('show-in-progress', 'show-live');
+      columns.classList.add('show-planned');
+      
       plannedHeader.classList.add('show-border-planned');
-    
-          //border  remove
       inProgressHeader.classList.remove('show-border-progress');
       liveHeader.classList.remove('show-border-live');
-    });
+  });
 
-    inProgressHeader.addEventListener('click', function () {
+  inProgressHeader.addEventListener('click', function () {
       columns.classList.remove('show-planned', 'show-live');
       columns.classList.add('show-in-progress');
-
-
-          //border add and remove
+      
       inProgressHeader.classList.add('show-border-progress');
       plannedHeader.classList.remove('show-border-planned');
       liveHeader.classList.remove('show-border-live');
-    });
+  });
 
-    liveHeader.addEventListener('click', function () {
+  liveHeader.addEventListener('click', function () {
       columns.classList.remove('show-planned', 'show-in-progress');
       columns.classList.add('show-live');
 
-
-        //border add and remove
       liveHeader.classList.add('show-border-live');
       inProgressHeader.classList.remove('show-border-progress');
       plannedHeader.classList.remove('show-border-planned');
-
-    });
   });
+});
