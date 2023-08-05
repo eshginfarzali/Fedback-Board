7499104a98bacda2be4fb216b98d0b70137bc0f4
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-08-05 22:50:05 +0400

    feat: add menu mobil

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,25 @@ function sortAndDisplay() {
 feedSort.addEventListener("change", sortAndDisplay);
 
 fetchAndRenderData();
+function updateLayout() {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    if (menuToggle.checked) {
+      body.style.overflow = "hidden";
+      roadmapBox.style.display = "inline-block";
+      allBox.style.display = "inline-block";
+    } else {
+      body.style.overflow = "auto";
+      roadmapBox.style.display = "none";
+      allBox.style.display = "none";
+    }
+  } else if (window.matchMedia("(min-width: 768px)").matches) {
+    body.style.overflow = "auto";
+    roadmapBox.style.display = "flex";
+    allBox.style.display = "flex";
+  }
+}
+
+menuToggle.addEventListener("change", updateLayout);
+window.addEventListener("resize", updateLayout);
+
+updateLayout();
