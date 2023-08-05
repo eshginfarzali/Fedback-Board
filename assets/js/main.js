965ed0b1304b3e35908b0408f582d495d4bd664c
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-08-05 19:09:22 +0400

    feat: sort feedback

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,13 @@ const body = document.body;
 const menuToggle = document.getElementById("menu__toggle");
 const roadmapBox = document.querySelector(".roadmap-box");
 const allBox = document.querySelector(".all-box");
+
+
+
+
 const feedBackBox = document.getElementById("feedBackBox");
 const feedsCount = document.querySelector(".feeds-count");
+const feedSort = document.getElementById("sort");
 
 const all = document.querySelector(".all");
 const ui = document.querySelector(".ui");
@@ -12,17 +17,21 @@ const enhancement = document.querySelector(".enhancement");
 const bug = document.querySelector(".bug");
 const feature = document.querySelector(".feature");
 
+let data = [];
+
 async function fetchAndRenderData() {
   try {
     const response = await fetch("../../Json/feedback.json");
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-
-    const data = await response.json();
+    
+    data = await response.json();
 
     feedBackBox.innerHTML = "";
+
     const statusCounts = {};
+
     data.forEach((item) => {
       const status = item.status;
       if (!statusCounts[status]) {
@@ -31,11 +40,10 @@ async function fetchAndRenderData() {
         statusCounts[status]++;
       }
 
-      console.log(statusCounts);
       feedsCount.innerHTML = `
-      <li>${statusCounts.Planned || 0}  </li>
-      <li>${statusCounts["In-Progress"] || 0}</li>
-      <li>${statusCounts.Live || 0}</li>`;
+        <li>${statusCounts.Planned || 0}</li>
+        <li>${statusCounts["In-Progress"] || 0}</li>
+        <li>${statusCounts.Live || 0}</li>`;
 
       const section = document.createElement("section");
       section.className = "add-feedback-box";
@@ -50,7 +58,7 @@ async function fetchAndRenderData() {
 
       const countReitin = document.createElement("span");
       countReitin.className = "countReitin";
-      countReitin.textContent = item.reiting;
+      countReitin.textContent = item.rating;
 
       countButton.appendChild(countImg);
       countButton.appendChild(document.createElement("br"));
@@ -96,8 +104,8 @@ async function fetchAndRenderData() {
       section.appendChild(textFeedBoxDiv);
 
       countButton.addEventListener("click", () => {
-        item.reiting++;
-        countReitin.textContent = item.reiting.toString();
+        item.rating++;
+        countReitin.textContent = item.rating.toString();
       });
 
       feedBackBox.appendChild(section);
@@ -106,8 +114,6 @@ async function fetchAndRenderData() {
     console.error("Error fetching data:", error);
   }
 }
-
-fetchAndRenderData();
 
 function filterAndDisplay(category) {
   const feedbackSections = feedBackBox.querySelectorAll(".add-feedback-box");
@@ -128,27 +134,98 @@ enhancement.addEventListener("click", () => filterAndDisplay("Enhancement"));
 bug.addEventListener("click", () => filterAndDisplay("Bug"));
 feature.addEventListener("click", () => filterAndDisplay("Feature"));
 
-fetchAndRenderData();
+function sortAndDisplay() {
+  const value = feedSort.options[feedSort.selectedIndex].value;
+  console.log(value);
 
-function updateLayout() {
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    if (menuToggle.checked) {
-      body.style.overflow = "hidden";
-      roadmapBox.style.display = "inline-block";
-      allBox.style.display = "inline-block";
-    } else {
-      body.style.overflow = "auto";
-      roadmapBox.style.display = "none";
-      allBox.style.display = "none";
-    }
-  } else if (window.matchMedia("(min-width: 768px)").matches) {
-    body.style.overflow = "auto";
-    roadmapBox.style.display = "flex";
-    allBox.style.display = "flex";
+  switch (value) {
+    case "mostcom":
+      data.sort((a, b) => (b.comment || 0) - (a.comment || 0));
+      break;
+    case "leastcom":
+      data.sort((a, b) => (a.comment || 0) - (b.comment || 0));
+      break;
+    case "mostup":
+      data.sort((a, b) => (b.rating || 0) - (a.rating || 0));
+      break;
+    case "leastup":
+      data.sort((a, b) => (a.rating || 0) - (b.rating || 0));
+      break;
+    default:
+      break;
   }
+
+  feedBackBox.innerHTML = ""; // Clear the existing content
+
+  data.forEach((item) => {
+    const section = document.createElement("section");
+    section.className = "add-feedback-box";
+
+    const countFeedDiv = document.createElement("div");
+    countFeedDiv.className = "count-feed";
+
+    const countButton = document.createElement("button");
+    const countImg = document.createElement("img");
+    countImg.src = "./assets/icons/chevron-up.svg";
+    countImg.alt = "up";
+
+    const countReitin = document.createElement("span");
+    countReitin.className = "countReitin";
+    countReitin.textContent = item.rating;
+
+    countButton.appendChild(countImg);
+    countButton.appendChild(document.createElement("br"));
+    countButton.appendChild(countReitin);
+    countFeedDiv.appendChild(countButton);
+
+    const textFeedBoxDiv = document.createElement("div");
+    textFeedBoxDiv.className = "text-feed-box";
+
+    const textFeedDiv = document.createElement("div");
+    textFeedDiv.className = "text-feed";
+
+    const titleHeading = document.createElement("h2");
+    titleHeading.textContent = item.title;
+
+    const textParagraph = document.createElement("p");
+    textParagraph.textContent = item.text;
+
+    const categoryButton = document.createElement("button");
+    categoryButton.textContent = item.category;
+
+    textFeedDiv.appendChild(titleHeading);
+    textFeedDiv.appendChild(textParagraph);
+    textFeedDiv.appendChild(categoryButton);
+
+    const feedComDiv = document.createElement("div");
+    feedComDiv.className = "feed-com";
+
+    const commentImg = document.createElement("img");
+    commentImg.src = "./assets/icons/comment.svg";
+    commentImg.alt = "comment";
+
+    const commentSpan = document.createElement("span");
+    commentSpan.textContent = item.comment;
+
+    feedComDiv.appendChild(commentImg);
+    feedComDiv.appendChild(commentSpan);
+
+    textFeedBoxDiv.appendChild(textFeedDiv);
+    textFeedBoxDiv.appendChild(feedComDiv);
+
+    section.appendChild(countFeedDiv);
+    section.appendChild(textFeedBoxDiv);
+
+    countButton.addEventListener("click", () => {
+      item.rating++;
+      countReitin.textContent = item.rating.toString();
+    });
+
+    feedBackBox.appendChild(section);
+  });
 }
 
-menuToggle.addEventListener("change", updateLayout);
-window.addEventListener("resize", updateLayout);
+feedSort.addEventListener("change", sortAndDisplay);
 
-updateLayout();
+fetchAndRenderData();
+ 
